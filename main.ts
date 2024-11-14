406e7eb389dd882498c5ea154302f80e3379aa05
gkn2 <112503427+gkn2@users.noreply.github.com>
2024-11-14 12:24:41 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,157 @@
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.vy += -13
+    animation.runImageAnimation(
+    mySprite,
+    [img`
+        . . . . . . 9 9 f f 9 9 . . . . 
+        . . . . . 9 f f f f f f 9 . . . 
+        . . . . f f f f f f f f f f . . 
+        . . . 9 f f 2 f f f f f f f 9 . 
+        . . . 8 f f 2 f f f f f f f 8 . 
+        . . . 8 f f 2 f f f f f f f 8 . 
+        . . . 8 f f 2 f f f f f f f 8 . 
+        . . . 8 f f 5 2 2 f f f f f 8 . 
+        . . . f f f 2 c c c c 2 f f f . 
+        . . . f f f c d d d d c f f f . 
+        . . . 1 1 f d d d d d d f 1 1 . 
+        . . . 1 1 f f f f f f f f 1 1 . 
+        . . . 1 1 d f f f f f f d 1 1 . 
+        . . . 1 1 2 d f f f f d 2 1 1 . 
+        . . . 1 1 f f f f f f f f 1 1 . 
+        . . . . 1 1 1 1 1 1 1 1 1 1 . . 
+        `,img`
+        . . . . . . 9 9 f f 9 9 . . . . 
+        . . . . . 9 f f f f f f 9 . . . 
+        . . . . f f f f f f f f f f . . 
+        . . . 9 f f 2 f f f f f f f 9 . 
+        . . . 8 f f 2 f f f f f f f 8 . 
+        . . . 8 f f 2 f f f f f f f 8 . 
+        . . . 8 f f 2 f f f f f f f 8 . 
+        . . . 8 f f 5 2 2 f f f f f 8 . 
+        . . . f f f 2 c c c c 2 f f f . 
+        . . . f f f c d d d d c f f f . 
+        . . . 1 1 f d d d d d d f 1 1 . 
+        . . . 1 1 f f f f f f f f 1 1 . 
+        . . . 1 1 1 1 1 1 1 1 1 1 1 1 . 
+        . . . 1 1 1 1 1 1 1 1 1 1 1 1 . 
+        . . . 1 1 1 1 1 1 1 1 1 1 1 1 . 
+        . . . . 1 1 1 1 1 1 1 1 1 1 . . 
+        `,img`
+        . . . . . . 9 9 f f 9 9 . . . . 
+        . . . . . 9 f f f f f f 9 . . . 
+        . . . . f f f f f f f f f f . . 
+        . . . 9 f f 2 f f f f f f f 9 . 
+        . . . 8 f f 2 f f f f f f f 8 . 
+        . . . 8 f f 2 f f f f f f f 8 . 
+        . . . 8 f f 2 f f f f f f f 8 . 
+        . . . 8 f f 5 2 2 f f f f f 8 . 
+        . . . f f f 2 c c c c 2 f f f . 
+        . . . 1 1 1 c d d d d c 1 1 1 . 
+        . . . 1 1 1 d d d d d d 1 1 1 . 
+        . . . 1 1 1 1 1 1 1 1 1 1 1 1 . 
+        . . . 1 1 1 1 1 1 1 1 1 1 1 1 . 
+        . . . 1 1 1 1 1 1 1 1 1 1 1 1 . 
+        . . . 1 1 1 1 1 1 1 1 1 1 1 1 . 
+        . . . . 1 1 1 1 1 1 1 1 1 1 . . 
+        `,img`
+        . . . . . . 9 9 f f 9 9 . . . . 
+        . . . . . 9 f f f f f f 9 . . . 
+        . . . . f f f f f f f f f f . . 
+        . . . 9 f f 2 f f f f f f f 9 . 
+        . . . 8 f f 2 f f f f f f f 8 . 
+        . . . 8 f f 2 f f f f f f f 8 . 
+        . . . 8 f f 2 f f f f f f f 8 . 
+        . . . 8 f f 5 2 2 f f f f f 8 . 
+        . . . f f f 2 c c c c 2 f f f . 
+        . . . 1 1 1 c d d d d c 1 1 1 . 
+        . . . 1 1 1 d d d d d d 1 1 1 . 
+        . . . 1 1 1 1 1 1 1 1 1 1 1 1 . 
+        . . . 1 1 1 1 1 1 1 1 1 1 1 1 . 
+        . . . 1 2 2 2 2 1 1 2 2 2 2 1 . 
+        . . . 1 2 4 4 2 1 1 2 4 4 2 1 . 
+        . . . . 2 1 1 2 1 1 2 1 1 2 . . 
+        `,img`
+        . . . . . . 9 9 f f 9 9 . . . . 
+        . . . . . 9 f f f f f f 9 . . . 
+        . . . . f f f f f f f f f f . . 
+        . . . 9 f f 2 f f f f f f f 9 . 
+        . . . 8 f f 2 f f f f f f f 8 . 
+        . . . 8 f f 2 f f f f f f f 8 . 
+        . . . 8 f f 2 f f f f f f f 8 . 
+        . . . 8 f f 5 2 2 f f f f f 8 . 
+        . . . f f f 2 c c c c 2 f f f . 
+        . . . 1 1 1 c d d d d c 1 1 1 . 
+        . . . 1 1 1 d d d d d d 1 1 1 . 
+        . . . 1 1 2 2 1 1 1 1 2 2 1 1 . 
+        . . . 1 2 2 2 2 1 1 2 2 2 2 1 . 
+        . . . 2 4 4 4 4 2 2 4 4 4 4 2 . 
+        . . . 2 4 4 4 4 2 2 4 4 4 4 2 . 
+        . . . . 4 1 1 4 1 1 4 1 1 4 . . 
+        `,img`
+        . . . . . . 9 9 f f 9 9 . . . . 
+        . . . . . 9 f f f f f f 9 . . . 
+        . . . . f f f f f f f f f f . . 
+        . . . 9 f f 2 f f f f f f f 9 . 
+        . . . 8 f f 2 f f f f f f f 8 . 
+        . . . 8 f f 2 f f f f f f f 8 . 
+        . . . 8 f f 2 f f f f f f f 8 . 
+        . . . 8 f f 5 2 2 f f f f f 8 . 
+        . . . f f f 2 c c c c 2 f f f . 
+        . . . 4 4 4 c d d d d c 5 5 5 . 
+        . . . 4 5 5 5 d d d d d 5 5 4 . 
+        . . . 4 5 2 5 5 5 5 4 2 5 5 5 . 
+        . . . 4 2 2 2 2 5 5 2 2 2 2 5 . 
+        . . . 2 5 5 4 5 2 2 5 4 5 5 2 . 
+        . . . 2 4 5 5 5 2 2 5 5 4 5 2 . 
+        . . . 2 5 5 5 4 2 2 5 5 5 5 2 . 
+        `,img`
+        . . . . . . 9 9 f f 9 9 . . . . 
+        . . . . . 9 f f f f f f 9 . . . 
+        . . . . f f f f f f f f f f . . 
+        . . . 9 f f 2 f f f f f f f 9 . 
+        . . . 8 f f 2 f f f f f f f 8 . 
+        . . . 8 f f 2 f f f f f f f 8 . 
+        . . . 8 f f 2 f f f f f f f 8 . 
+        . . . 8 f f 5 2 2 f f f f f 8 . 
+        . . . f f f 2 c c c c 2 f f f . 
+        . . . f f f c d d d d c f f f . 
+        . . . f f f d d d d d d f f f . 
+        . . . f f f f f f f f f f f f . 
+        . . . 8 f d f f f f f f d f 8 . 
+        . . . 8 f 2 d f f f f d 2 f 8 . 
+        . . . 8 8 f f f f f f f f 8 8 . 
+        . . . . 8 8 . . . . . . 8 8 . . 
+        `],
+    67,
+    false
+    )
+    if (difficulty == 0) {
+        mySprite.vy += -20
+    }
+})
 sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Enemy, function (sprite, otherSprite) {
     sprites.destroy(car)
+})
+info.onCountdownEnd(function () {
+    game.gameOver(true)
+})
+function setDifficulty () {
+    if (difficulty == 0) {
+        spawninterval = 1800
+        info.startCountdown(20)
+    } else if (difficulty == 1) {
+        spawninterval = 1400
+        info.startCountdown(30)
+    } else if (difficulty == 2) {
+        spawninterval = 1000
+        info.startCountdown(40)
+    }
+}
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.vy += 13
+    if (difficulty == 0) {
+        mySprite.vy += 20
+    }
 })
 function SpawnRoadCars () {
     car = sprites.create(img`
@@ -27,80 +179,83 @@ function SpawnRoadCars () {
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     game.gameOver(false)
+    pause(100)
     music.play(music.createSoundEffect(WaveShape.Square, 200, 1, 255, 70, 100, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.InBackground)
 })
+let spawninterval = 0
 let car: Sprite = null
+let difficulty = 0
+let mySprite: Sprite = null
 let RoadCars: Image[] = []
 RoadCars = [img`
-    . . . . . . e e c c e e . . . . 
+    . . . . . . e e a a e e . . . . 
     . . . . . e 2 2 2 2 2 2 e . . . 
-    . . . . 2 c 2 2 2 2 2 2 c 2 . . 
-    . . . e 2 c 4 2 2 2 2 2 c 2 e . 
-    . . . f 2 2 4 2 2 2 2 2 c 2 f . 
-    . . . f 2 2 4 2 2 2 2 2 2 2 f . 
-    . . . f 2 2 4 2 2 2 2 2 2 2 f . 
-    . . . f 2 c 2 4 4 2 2 2 c 2 f . 
-    . . . e 2 c e c c c c e c 2 e . 
-    . . . e 2 e c b b b b c e 2 e . 
-    . . . e 2 e b b b b b b e 2 e . 
+    . . . . 2 a 2 2 2 2 2 2 a 2 . . 
+    . . . e 2 a 3 2 2 2 2 2 a 2 e . 
+    . . . f 2 2 3 2 2 2 2 2 a 2 f . 
+    . . . f 2 2 3 2 2 2 2 2 2 2 f . 
+    . . . f 2 2 3 2 2 2 2 2 2 2 f . 
+    . . . f 2 a 2 3 3 2 2 2 a 2 f . 
+    . . . e 2 a e a a a a e a 2 e . 
+    . . . e 2 e a 1 1 1 1 a e 2 e . 
+    . . . e 2 e 1 1 1 1 1 1 e 2 e . 
     . . . e e e e e e e e e e e e . 
     . . . f e d e e e e e e d e f . 
     . . . f e 2 d e e e e d 2 e f . 
     . . . f f e e e e e e e e f f . 
     . . . . f f . . . . . . f f . . 
     `, img`
-    . . . . . . 8 8 c c 8 8 . . . . 
-    . . . . . 8 6 6 6 6 6 6 8 . . . 
-    . . . . 6 c 6 6 6 6 6 6 c 6 . . 
-    . . . 8 6 c 9 6 6 6 6 6 c 6 8 . 
-    . . . f 6 6 9 6 6 6 6 6 c 6 f . 
-    . . . f 6 6 9 6 6 6 6 6 6 6 f . 
-    . . . f 6 6 9 6 6 6 6 6 6 6 f . 
-    . . . f 6 c 6 9 9 6 6 6 c 6 f . 
-    . . . 8 6 c 8 c c c c 8 c 6 8 . 
-    . . . 8 6 8 c b b b b c 8 6 8 . 
-    . . . 8 6 8 b b b b b b 8 6 8 . 
-    . . . 8 8 8 8 8 8 8 8 8 8 8 8 . 
-    . . . f 8 d 8 8 8 8 8 8 d 8 f . 
-    . . . f 8 6 d 8 8 8 8 d 6 8 f . 
-    . . . f f 8 8 8 8 8 8 8 8 f f . 
+    . . . . . . e e 1 1 e e . . . . 
+    . . . . . e d d d d d d e . . . 
+    . . . . d 1 d d d d d d 1 d . . 
+    . . . e d 1 1 d d d d d 1 d e . 
+    . . . f d d 1 d d d d d 1 d f . 
+    . . . f d d 1 d d d d d d d f . 
+    . . . f d d 1 d d d d d d d f . 
+    . . . f d 1 1 1 1 d d d 1 d f . 
+    . . . 1 d 1 1 1 1 1 1 1 1 d 1 . 
+    . . . 1 d 1 1 b b b b 1 1 d 1 . 
+    . . . 1 d 1 b b b b b b 1 d 1 . 
+    . . . 1 1 1 1 1 1 1 1 1 1 1 1 . 
+    . . . f 1 d 1 1 1 1 1 1 d 1 f . 
+    . . . f 1 1 2 1 1 1 1 2 1 1 f . 
+    . . . f f 1 1 1 1 1 1 1 1 f f . 
     . . . . f f . . . . . . f f . . 
     `, img`
-    . . . . . . a a c c a a . . . . 
-    . . . . . a 3 3 3 3 3 3 a . . . 
-    . . . . 3 c 3 3 3 3 3 3 c 3 . . 
-    . . . a 3 c d 3 3 3 3 3 c 3 a . 
-    . . . f 3 3 d 3 3 3 3 3 c 3 f . 
-    . . . f 3 3 d 3 3 3 3 3 3 3 f . 
-    . . . f 3 3 d 3 3 3 3 3 3 3 f . 
-    . . . f 3 c 3 d d 3 3 3 c 3 f . 
-    . . . a 3 c a c c c c a c 3 a . 
-    . . . a 3 a c b b b b c a 3 a . 
-    . . . a 3 a b b b b b b a 3 a . 
-    . . . a a a a a a a a a a a a . 
-    . . . f a d a a a a a a d a f . 
-    . . . f a 3 d a a a a d 3 a f . 
-    . . . f f a a a a a a a a f f . 
+    . . . . . . 8 8 7 7 8 8 . . . . 
+    . . . . . 8 6 6 6 6 6 6 8 . . . 
+    . . . . 6 7 6 6 6 6 6 6 7 6 . . 
+    . . . 8 6 7 9 6 6 6 6 6 7 6 8 . 
+    . . . f 6 6 9 6 6 6 6 6 7 6 f . 
+    . . . f 6 6 9 6 6 6 6 6 6 6 f . 
+    . . . f 6 6 9 6 6 6 6 6 6 6 f . 
+    . . . f 6 7 6 9 9 6 6 6 7 6 f . 
+    . . . 8 6 7 8 7 7 7 7 8 7 6 8 . 
+    . . . 8 6 8 7 b b b b 7 8 6 8 . 
+    . . . 8 6 8 b b b b b b 8 6 8 . 
+    . . . 8 8 8 8 8 8 8 8 8 8 8 8 . 
+    . . . f 8 c 8 8 8 8 8 8 c 8 f . 
+    . . . f 8 5 c 8 8 8 8 c 5 8 f . 
+    . . . f f 8 8 8 8 8 8 8 8 f f . 
     . . . . f f . . . . . . f f . . 
     `]
-let spawninterval = 2000
-let mySprite = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . b 5 5 b . . . 
-    . . . . . . b b b b b b . . . . 
-    . . . . . b b 5 5 5 5 5 b . . . 
-    . b b b b b 5 5 5 5 5 5 5 b . . 
-    . b d 5 b 5 5 5 5 5 5 5 5 b . . 
-    . . b 5 5 b 5 d 1 f 5 d 4 f . . 
-    . . b d 5 5 b 1 f f 5 4 4 c . . 
-    b b d b 5 5 5 d f b 4 4 4 4 b . 
-    b d d c d 5 5 b 5 4 4 4 4 4 4 b 
-    c d d d c c b 5 5 5 5 5 5 5 b . 
-    c b d d d d d 5 5 5 5 5 5 5 b . 
-    . c d d d d d d 5 5 5 5 5 d b . 
-    . . c b d d d d d 5 5 5 b b . . 
-    . . . c c c c c c c c b b . . . 
+mySprite = sprites.create(img`
+    . . . . . . 9 9 f f 9 9 . . . . 
+    . . . . . 9 f f f f f f 9 . . . 
+    . . . . f f f f f f f f f f . . 
+    . . . 9 f f 2 f f f f f f f 9 . 
+    . . . 8 f f 2 f f f f f f f 8 . 
+    . . . 8 f f 2 f f f f f f f 8 . 
+    . . . 8 f f 2 f f f f f f f 8 . 
+    . . . 8 f f 5 2 2 f f f f f 8 . 
+    . . . f f f 2 c c c c 2 f f f . 
+    . . . f f f c d d d d c f f f . 
+    . . . f f f d d d d d d f f f . 
+    . . . f f f f f f f f f f f f . 
+    . . . 8 f d f f f f f f d f 8 . 
+    . . . 8 f 2 d f f f f d 2 f 8 . 
+    . . . 8 8 f f f f f f f f 8 8 . 
+    . . . . 8 8 . . . . . . 8 8 . . 
     `, SpriteKind.Player)
 mySprite.setPosition(80, 100)
 controller.moveSprite(mySprite, 100, 0)
@@ -228,6 +383,11 @@ scene.setBackgroundImage(img`
     `)
 scroller.scrollBackgroundWithCamera(scroller.CameraScrollMode.OnlyVertical)
 scroller.scrollBackgroundWithSpeed(0, 80)
+difficulty = 3
+while (difficulty > 2) {
+    difficulty = game.askForNumber("Choose difficulty: 0 to 2. Other numbers invalid.", 1)
+}
+setDifficulty()
 game.onUpdateInterval(spawninterval, function () {
     SpawnRoadCars()
 })
